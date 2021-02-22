3bcc0a5d1457eef7c43c0ffcedb0c47d30cdd2b3
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-22 19:31:26 -0300

    Add error messages

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("form")
-let validate = false
+let validate
 
 const printAlert = (message, element) =>{
    const alert = document.createElement("P")
@@ -9,17 +9,30 @@ const printAlert = (message, element) =>{
    element.insertAdjacentElement("afterend", alert)
 }
 
-const validateForm = element =>{
+const validateEmail = (element, message) =>{
+   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+   if(!re.test(element.value.toLowerCase())){
+      element.classList.add("form__input--error")
+      !element.nextElementSibling.classList.contains("form__error") && printAlert(message, element)
+      validate = false
+   }else{
+      element.classList.remove("form__input--error")
+      if(element.nextElementSibling.classList.contains("form__error"))
+         element.nextElementSibling.remove()
+   }
+
+}
+
+const validateForm = (element, message) =>{
       if(!element.value.trim().length > 0 || element.value == null){
          element.classList.add("form__input--error")
-         !element.nextElementSibling.classList.contains("form__error") && printAlert("First Name cannot be empty", element)
+         !element.nextElementSibling.classList.contains("form__error") && printAlert(message, element)
          validate = false
       }else{
          element.classList.remove("form__input--error")
-         if(element.nextElementSibling.classList.contains("form__error")){
+         if(element.nextElementSibling.classList.contains("form__error"))
             element.nextElementSibling.remove()
-         }   
-         validate = true
       }
 
    
@@ -28,10 +41,12 @@ const validateForm = element =>{
 form.addEventListener("submit", e =>{
    e.preventDefault()
 
-   validateForm(form.firstname)
-   validateForm(form.lastname)
-   validateForm(form.email)
-   validateForm(form.password)
+   validate = true
+
+   validateForm(form.firstname, "First Name cannot be empty")
+   validateForm(form.lastname, "Last Name cannot be empty")
+   validateEmail(form.email, "Looks like this is not an email")
+   validateForm(form.password, "Password cannot be empty")
 
    validate && form.reset()
 })
